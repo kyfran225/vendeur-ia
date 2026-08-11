07ceab9a1a224cbb525053cc5210fa47571c38d0
Koutouan <kyfran6@gmail.com>
2026-08-11 23:22:22 +0000

    chore: update service worker assets

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sh7cpjql9no"
+    "revision": "0.7mv16r2j7s8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

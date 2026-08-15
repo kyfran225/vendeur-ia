27104378428b036557cecfec3964a7b96a39a54a
Koutouan <kyfran6@gmail.com>
2026-08-15 00:53:34 +0000

    feat: enhance Gemini configuration and commerce service, update sw and settings UI

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lr6mm7c6g2"
+    "revision": "0.j89q3hucisg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
